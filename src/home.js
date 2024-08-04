b58f15bf15804549803428d4d016177d6211003d
Zach4531 <zciatteo96@gmail.com>
2024-08-03 23:03:39 -0400

    Add home page

--- src/home.js
+++ src/home.js
@@ -11,21 +11,47 @@ export function Home() {
   headlineContainer.classList.add('home-heading');
 
   const headline = document.createElement('h1');
-  headline.textContent = 'Welcome to TheBean';
+  headline.textContent = 'TheBean';
 
   const subHeadline = document.createElement('h2');
-  subHeadline.textContent = 'Brewed to Perfection, Just for You';
+  subHeadline.textContent = 'Awaken Your Senses with Every Sip';
 
   const menuButton = document.createElement('button');
   menuButton.classList.add('home-button');
-  menuButton.textContent = 'View Menu';
+  menuButton.textContent = 'View Our Menu';
 
   headlineContainer.appendChild(logo);
   headlineContainer.appendChild(headline);
   headlineContainer.appendChild(subHeadline);
   headlineContainer.appendChild(menuButton);
 
+  const sectionContainer = document.createElement('div');
+  sectionContainer.classList.add('section-one', 'container');
+
+  const sectionMainHeading = document.createElement('h1');
+  sectionMainHeading.textContent = 'Who We Are';
+
+  const sectionImageContainer = document.createElement('div');
+  sectionImageContainer.classList.add('section-one-img');
+
+  const sectionContentContainer = document.createElement('div');
+
+  const sectionHeadingOne = document.createElement('h2');
+  sectionHeadingOne.textContent = 'Brewed to Perfection, Just for You';
+
+  const sectionHeadingText = document.createElement('p');
+  sectionHeadingText.textContent =
+    'At our coffee shop, we take pride in delivering the perfect cup of coffee tailored to your taste. Our expert baristas meticulously select and roast the finest beans, ensuring every sip is a testament to quality and craftsmanship. Whether you prefer a bold espresso or a smooth latte, each beverage is brewed to perfection just for you. Step into our cozy atmosphere, savor the rich flavors, and let us make your coffee experience truly exceptional.';
+
+  sectionContentContainer.appendChild(sectionHeadingOne);
+  sectionContentContainer.appendChild(sectionHeadingText);
+
+  sectionContainer.appendChild(sectionMainHeading);
+  sectionContainer.appendChild(sectionImageContainer);
+  sectionContainer.appendChild(sectionContentContainer);
+
   container.appendChild(headlineContainer);
+  container.appendChild(sectionContainer);
 
   return container;
 }
